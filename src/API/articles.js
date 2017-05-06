3992ef2c2dd0226c149d0ebb5387bdedde83ee1b
T Rahul Thampi <trahulthampi@gmail.com>
2017-05-06 18:26:49 +0530

    articles api end declaration complete

--- src/API/articles.js
+++ src/API/articles.js
@@ -1,3 +1,22 @@
-const getArticles = () => { };
+const getArticles = (selectedSource) => {
+  const API_KEY = 'ae05d1e02d2046b48a4e44223b397237';
+  const ARTICLES_API_ENDPOINT = `https://newsapi.org/v1/articles?source=${selectedSource}&apiKey=${API_KEY}`;
+  const fetchParams = {
+    method: 'GET',
+  };
+
+  return fetch(ARTICLES_API_ENDPOINT, fetchParams)
+    .then((response) => {
+      console.log('response > ', response);
+      return response.json();
+    })
+    .then((json) => {
+      console.log('json > ', json);
+    })
+    .catch(error => ({
+      status: 200,
+      message: error.message,
+    }));
+};
 
 export default getArticles;
